969fe2083765203ce4dd0aa94e8d01ced272b917
Christophe Ameller <christophe@getjoe.co>
2021-10-28 14:49:14 +0200

    data binding with states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [value, setValue] = useState('');
+  const [touched, setTouched] = useState(false);
+  const [valid, setValid] = useState(false);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    setValid(value.length <= 20);
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={e => {
+        e.preventDefault();
+        const isValid = touched && valid;
+        if (isValid) {
+          setName(value);
+          setValue('');
+        }
+      }}>
+        <input type="text" value={value} onChange={e => {
+          setTouched(true);
+          setValue(e.target.value);
+        }}/>
+      </form>
+      <p>My name is {value}</p>
+      <p>name : {name}</p>
+      <p className={`small${!valid ? 'Red' : ''}Text`}>You are allowed a max of 20 characters</p>
     </div>
   );
 }
